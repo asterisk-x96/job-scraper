70d8ce9152cc195b3aa42a89e1c488b73e994fe0
Thao Pham <thachthao.6996@gmail.com>
2024-04-22 16:09:51 +0100

    Fixed fail to send keys to field

--- pages/collect.py
+++ pages/collect.py
@@ -3,8 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
-from selenium.webdriver.common.action_chains import ActionChains
-
+from selenium.webdriver.common.keys import Keys
+import time
 
 driver = webdriver.Chrome()
 
@@ -14,42 +14,42 @@ SEARCH_QUERY = {
 }
 
 def login(driver):
-    driver.get("https://www.linkedin.com")
-    while True:
-        try:
-            WebDriverWait(driver, 20).until(EC.url_contains("feed"))
-            print("login successful")
-            return True
-        except TimeoutException: 
-            return False
-
-def search_jobs(driver):
     try:
-        WebDriverWait(driver, 30).until(EC.url_contains("/jobs"))
-        position_field = WebDriverWait(driver, 30).until(
-            EC.visibility_of_element_located((By.CSS_SELECTOR, "input[id='jobs-search-box-keyword-id-ember1002']"))
-        )
-        location_field = WebDriverWait(driver, 30).until(
-            EC.visibility_of_element_located((By.CSS_SELECTOR, "input[id='jobs-search-box-location-id-ember1002']"))
-        )
-        
-        # Fill in location and position field with predefined data
-        position_field.send_keys(SEARCH_QUERY['position'])
-        ActionChains(driver).move_by_offset(0, 0).click()
-        location_field.send_keys(SEARCH_QUERY['location'])
-
-        try:
-            driver.find_element_by_xpath("//li/button[text()='United Kingdom']").click()
-        except NoSuchElementException:
-            pass
-        
+        driver.get("https://www.linkedin.com")
+        WebDriverWait(driver, 20).until(EC.url_contains("feed"))
+        print("Login successful")
         return True
-    
-    except TimeoutException:
+    except TimeoutException: 
+        print("Login failed: Timeout")
         return False
 
-        
+def search_jobs(driver):
+    # try:
+    WebDriverWait(driver, 30).until(EC.url_contains("/jobs"))
+    position_field = WebDriverWait(driver, 30).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "input[class='jobs-search-box__text-input jobs-search-box__keyboard-text-input']"))
+    )
+    location_field = WebDriverWait(driver, 30).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "input[class='jobs-search-box__text-input']"))
+    )
+    
+    # Fill in location and position field with predefined data
+    position_field.send_keys(SEARCH_QUERY['position'])
+    location_field.send_keys(SEARCH_QUERY['location'])
+    time.sleep(1)
+    location_field.send_keys(Keys.ENTER)
+    
+    # driver.find_element(By.XPATH, "//html").click()
 
 
-login(driver)
-search_jobs(driver)
+
+
+    
+    """ except TimeoutException:
+        print("Search jobs failed: Timeout")
+        return False """
+
+if login(driver):
+    search_jobs(driver)
+    time.sleep(300)
+    driver.quit()
